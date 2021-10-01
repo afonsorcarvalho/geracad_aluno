015fba109a8a513677561b84e490ae6aba25c5dc
Afonso Carvalho <afonso@diagnostica-ma.com.br>
2021-10-01 10:53:44 -0300

    backup

--- models/res_partner.py
+++ models/res_partner.py
@@ -9,15 +9,22 @@ class geracad_aluno(models.Model):
     _inherit = ['res.partner']
 
     name = fields.Char()
-    value = fields.Integer()
+    
     nome_pai = fields.Char("Nome do Pai")
     nome_mae = fields.Char("Nome da mãe")
     data_nascimento = fields.Date("Data de Nascimento")
     escolaridade = fields.Char()
+    rg = fields.Char("RG")
+    sexo = fields.Selection([('M', 'Masculino'), ('F', 'Feminino'), ('O', 'Outros')])
+    
 
 
     e_aluno = fields.Boolean(string='É aluno')
+    e_professor = fields.Boolean(string='É professor')
+    ponto_referencia_endereco = fields.Char("Ponto de Referência")
     
     
     observacoes = fields.Text("Observações")
+
+    
    
